407434a3035fed62f6738ace2a588680ed680457
zhydani <zhydani@gmail.com>
2020-11-19 11:24:32 -0300

    apagando projeteis antigos

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,6 +31,12 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        # apaga projetei s que desaparecem
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
         gf.update_screen(ai_settings, screen, ship, bullets)
 
 
