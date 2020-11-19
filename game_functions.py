e4c323ed0487c3c53aa3336d9c099656a7cdb555
zhydani <zhydani@gmail.com>
2020-11-18 21:58:08 -0300

    refatorando o codigo para metodo de keydown e keyup

--- game_functions.py
+++ game_functions.py
@@ -3,22 +3,32 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    # responde a pressionamento de teclas
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    # responde a solturas de tecla
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     # responde a eventos de pressionamento de teclas e de mouse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
