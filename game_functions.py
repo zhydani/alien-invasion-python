54dfeda64439502fa2549eb103d8c45d6c8c9cf8
zhydani <zhydani@gmail.com>
2020-11-19 22:52:50 -0300

    refatorando o codigo

--- game_functions.py
+++ game_functions.py
@@ -72,17 +72,25 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
+def get_number_aliens_x(ai_settings, alien_width):
+    # determina o numero de alienigenas que cabem em uma linha
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    # cria um alienigena e o posiciona na linha
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
 def create_fleet(ai_settings, screen, aliens):
     # cria uma frota completa de alienigena
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    avaliable_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(avaliable_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
 
     # cria a primeira linha de alienigenas
     for alien_number in range(number_aliens_x):
-        # cria um alienigena e o posiciona na linha
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
