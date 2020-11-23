fd9795a6d3234fc436f0b012aa307488bc31b174
zhydani <zhydani@gmail.com>
2020-11-23 20:56:45 -0300

    verificando se um alienigena atingiu a borda

--- alien.py
+++ alien.py
@@ -24,8 +24,16 @@ class Alien(Sprite):
     def blitme(self):
         # desenha o alienigena em sua posicao atual 
         self.screen.blit(self.image, self.rect)
+    
+    def check_edges(self):
+        # devolve True se o alienigena estiver na borda da tela
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
 
     def update(self):
         # Move o alienigena para a direita
-        self.x += self.ai_settings.alien_speed_factor
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
         self.rect.x = self.x
